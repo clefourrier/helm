ca5c0bff117489062c04ad6097cc41d403a21e02
Clémentine <cle.fourrier@gmail.com>
2023-02-15 09:32:22 +0100

    Simplifying the code base aggressively
    Keeping the scenarios, removing all proxi/API code, token counters (which are useless from free calls to model), might need to add scripts/fact_completion back at one point

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
-<!--intro-start-->
-# Holistic Evaluation of Language Models
+# HELM on the go
+Plug and play HELM
 
-[comment]: <> (When using the img tag, which allows us to specify size, src has to be a URL.)
-<img src="https://github.com/stanford-crfm/helm/raw/main/src/helm/benchmark/static/images/helm-logo.png" alt=""  width="800"/>
-
-Welcome! The **`crfm-helm`** Python package contains code used in the **Holistic Evaluation of Language Models** project ([paper](https://arxiv.org/abs/2211.09110), [website](https://crfm.stanford.edu/helm/v1.0/)) by [Stanford CRFM](https://crfm.stanford.edu/). This package includes the following features:
-
-- Collection of datasets in a standard format (e.g., NaturalQuestions)
-- Collection of models accessible via a unified API (e.g., GPT-3, MT-NLG, OPT, BLOOM)
-- Collection of metrics beyond accuracy (efficiency, bias, toxicity, etc.)
-- Collection of perturbations for evaluating robustness and fairness (e.g., typos, dialect)
-- Modular framework for constructing prompts from datasets
-- Proxy server for managing accounts and providing unified interface to access models
-<!--intro-end-->
-
-To get started, refer to [the documentation on Read the Docs](https://crfm-helm.readthedocs.io/) for how to install and run the package.
+## How to
+1) Define specs file in `run_spec.conf`. 
+	- Toy example: ` echo 'entries: [{description: "mmlu:subject=philosophy,model=geclm/gpt2_local", priority: 1}]' > run_specs.conf`
+	- Better examples in `benchmark/presentation/*.conf`
+2) From root: `python -m benchmark.run --conf-paths run_specs.conf --local --suite v1 --max-eval-instances 10 -n 1`
--- benchmark/metrics/tokens_metric.py
+++ benchmark/metrics/tokens_metric.py
@@ -9,15 +9,12 @@ from helm.benchmark.metrics.statistic import Stat, merge_stat
 from .metric import Metric, MetricResult, PerInstanceStats
 from .metric_name import MetricName
 from .metric_service import MetricService
-from .tokens.auto_token_cost_estimator import AutoTokenCostEstimator
-from .tokens.token_cost_estimator import TokenCostEstimator
 
 
 @dataclass
 class Processor:
     """Processes a single example."""
 
-    token_cost_estimator: TokenCostEstimator
     metric_service: MetricService
 
     def process(self, request_state: RequestState) -> List[Stat]:
@@ -25,10 +22,6 @@ class Processor:
         stats: List[Stat] = []
 
         # Estimated cost in terms of number of tokens
-        estimate_num_tokens_cost: int = self.token_cost_estimator.estimate_tokens(request, self.metric_service)
-        # TODO: replace with "estimated_num_tokens" - is this for prompt or completion
-        stats.append(Stat(MetricName("estimated_num_tokens_cost")).add(estimate_num_tokens_cost))
-
         stats.append(Stat(MetricName("num_completions")).add(request.num_completions))
 
         # Maximum number of tokens in the completions
@@ -44,9 +37,6 @@ class TokensMetric(Metric):
     Estimates the total number of tokens that will be used based on the requests.
     """
 
-    def __init__(self):
-        self.token_cost_estimator = AutoTokenCostEstimator()
-
     def __repr__(self):
         return "TokensMetric()"
 
@@ -60,7 +50,7 @@ class TokensMetric(Metric):
         """
         Add up all the estimated number of tokens used for each request.
         """
-        processor = Processor(token_cost_estimator=self.token_cost_estimator, metric_service=metric_service)
+        processor = Processor(metric_service=metric_service)
         results: List[List[Stat]] = parallel_map(
             processor.process,
             scenario_state.request_states,
